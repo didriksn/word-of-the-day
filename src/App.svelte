9192c347c2a653560dbafe0073c2952fbcadcbc7
didrik <didrik.sommersten@gmail.com>
2023-06-30 22:41:08 +0200

    Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,44 @@
 <script>
+  import { onMount } from 'svelte';
 
+  let quote;
+
+  const fetchRandomQuote = async () => {
+    const response = await fetch('src/quotes.json');
+    const data = await response.json();
+    const randomIndex = Math.floor(Math.random() * data.length);
+    quote = data[randomIndex];
+    localStorage.setItem('quote', JSON.stringify(quote));
+  };
+
+  onMount(() => {
+    const storedQuote = localStorage.getItem('quote');
+    if (storedQuote) {
+      quote = JSON.parse(storedQuote);
+    } else {
+      fetchRandomQuote(); // Initial fetch
+    }
+
+    const interval = setInterval(fetchRandomQuote, 10000); // Fetch a new random quote every 10 seconds (for testing local storage)
+
+    // Clean up the interval when the component is destroyed
+    return () => {
+      clearInterval(interval);
+    };
+  });
 </script>
 
 <main>
-  <div class="div" style="width: 200px; height: 200px;"></div>
+  {#if quote}
+    <div class="quote">
+      <p class="text">{quote.text}</p>
+      <p class="author">- {quote.author}</p>
+    </div>
+  {:else}
+    <p>Loading...</p>
+  {/if}
 </main>
+
+<style>
+  /* Add CSS styles for the main App component */
+</style>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/svelte-github-pages-template/", // name of your root directory
+  base: "/word-of-the-day/", // name of your root directory
   plugins: [svelte()]
 })
